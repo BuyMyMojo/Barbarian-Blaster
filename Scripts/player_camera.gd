f67675866ea8d7d615b2852fbb393a21c96d9dbe
BuyMyMojo <hello@buymymojo.net>
2024-07-03 19:43:35 +1000

    Mouse Input and Ray Picking

---
+++ Scripts/player_camera.gd
@@ -0,0 +1,12 @@
+extends Camera3D
+
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+
+
+func _process(delta: float) -> void:
+	var mouse_position: Vector2 = get_viewport().get_mouse_position()
+	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * 100
+	ray_cast_3d.force_raycast_update()
+	
+	if Engine.get_frames_drawn() % 4:
+		Log.log(ray_cast_3d.get_collider(), ray_cast_3d.get_collision_point())
