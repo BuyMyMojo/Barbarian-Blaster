16de2d8699c424b38e5e5c3d6294d44b34ba85a5
BuyMyMojo <hello@buymymojo.net>
2024-07-03 18:36:09 +1000

    Create base and enemy damaging base logic

---
+++ Scripts/base.gd
@@ -0,0 +1,7 @@
+class_name PlayerBase
+
+extends Node3D
+
+func take_damage() -> void:
+	Log.log("Base damaged")
+	pass
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,5 +3,12 @@ extends PathFollow3D
 ## Speed in m/s
 @export_range(0, 100) var speed: float = 6
 
+@onready var base: PlayerBase = get_tree().get_first_node_in_group("Base")
+
 func _process(delta: float) -> void:
 	progress += speed * delta
+	
+	if progress_ratio >= 1:
+		base.take_damage()
+		queue_free()
+
