fcfcba219f0cd7c402a8f20c1bac5af1be5fecdb
BuyMyMojo <hello@buymymojo.net>
2024-07-11 18:36:59 +1000

    Introducing Timers

--- Scripts/turret.gd
+++ Scripts/turret.gd
@@ -2,7 +2,17 @@ extends Node3D
 
 @export var projectile: PackedScene
 
-func fire_weapon():
+@onready var fire_point: Marker3D = $TurretBase/TurretTop/LauncherHolder/Axel/FirePoint
+
+var projectiles_fired: int = 0
+
+
+func fire_weapon() -> void:
 	var new_projectile: Node3D = projectile.instantiate()
 	add_child(new_projectile)
-	new_projectile.position = position
+	new_projectile.global_position = fire_point.global_position
+
+
+func _on_fire_timer_timeout() -> void:
+	fire_weapon()
+	projectiles_fired += 1
